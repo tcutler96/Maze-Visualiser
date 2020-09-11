500b60bada8e609b2fe7b30bad8b1df5bbf8565d
Thomas Cutler <67102917+tcutler96@users.noreply.github.com>
2020-09-11 23:25:57 +0100

    Update maze_visualiser.py

--- maze_visualiser.py
+++ maze_visualiser.py
@@ -6,6 +6,21 @@ import random
 import time
 import queue
 
+'''
+A Python script that first generates a maze and then solves it. This is visualised with PyGame using a simple but 
+effective design. Includes a main menu teaching the controls and offering the user the ability to change a number of 
+options that effect maze generation and solving. Once initiated, the current state of each cell is shown using colours, 
+which are explained with a handy key.
+
+**Options**
+* Maze Generation
+    * Queue Method (what order elements are taken out of the queue): LIFO, FIFO
+    * Choose Method (how new unvisited neighbours are chosen): random, first, last
+    * (Note: choosing LIFO and random yields the recursive back tracker algorithm)
+* Maze Solving
+    * Algorithm (which path finding algorithm is used): breadth-first search, depth-first search
+'''
+
 pg.init()
 
 width, height = 101, 51  # height and width and maze
